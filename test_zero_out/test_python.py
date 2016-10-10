97224e824f1f2c409531182b2b7f411a66335f69
David Liebman <radiodee1@gmail.com>
2016-10-10 13:42:02 -0400

    moved test_python.py

--- test_zero_out/test_python.py
+++ test_zero_out/test_python.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-zero_out_module = tf.load_op_library('zero_out.so')
+zero_out_module = tf.load_op_library('tensorflow/core/user_ops/zero_out.so')
 
 with tf.Session(''):
   print(zero_out_module.zero_out([[1, 2], [3, 4]]).eval())
