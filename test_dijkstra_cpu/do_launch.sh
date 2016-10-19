24d70a839768cdd658b7b5e4f5a2c94c2e70dfed
David Liebman <radiodee1@gmail.com>
2016-10-19 12:10:35 -0400

    rename test file

--- test_dijkstra_cpu/do_launch.sh
+++ test_dijkstra_cpu/do_launch.sh
@@ -1,5 +1,5 @@
-#./test_python.py -height 8 -width 8  -stopx 7 -stopy 6 -startx 4 -starty 0 -w-height 20.5 -randomize-floor
+#./test_op.py -height 8 -width 8  -stopx 7 -stopy 6 -startx 4 -starty 0 -w-height 20.5 -randomize-floor
 
-./test_python.py -mapname map.png -width 80 -height 80 -startx 65 -starty 74 -stopx 35 -stopy 9 
+#./test_op.py -mapname map.png -width 80 -height 80 -startx 65 -starty 74 -stopx 35 -stopy 9 
 
-#./test_python.py -mapname map.png -width 80 -height 80
+./test_op.py -mapname map.png -width 80 -height 80
