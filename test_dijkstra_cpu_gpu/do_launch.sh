7f4bf6a9e6c3977fe4af1c99f032a06cb0259b65
David Liebman <radiodee1@gmail.com>
2016-10-19 13:41:33 -0400

    rename following decision to work out gpu code with cpu code is dijkstra project

--- test_dijkstra_cpu_gpu/do_launch.sh
+++ test_dijkstra_cpu_gpu/do_launch.sh
@@ -1,7 +1,10 @@
+## test with character maze of 8x8
 #./test_op.py -height 8 -width 8  -stopx 7 -stopy 6 -startx 4 -starty 0 -w-height 20.5 -randomize-floor
 
+## takes less than a second on cpu
 #./test_op.py -mapname map.png -width 80 -height 80 -startx 65 -starty 74 -stopx 35 -stopy 9 
 
+## takes less than a second on cpu
 #./test_op.py -mapname map.png -width 80 -height 80
 
 ## takes at least 2min 21sec on cpu
