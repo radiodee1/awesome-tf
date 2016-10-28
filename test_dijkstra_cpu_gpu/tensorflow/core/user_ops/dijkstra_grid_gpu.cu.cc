5f12d5cb329313f456d819467dbe7bf8a15e82ae
David Liebman <radiodee1@gmail.com>
2016-10-28 17:08:13 -0400

    race conditions

--- test_dijkstra_cpu_gpu/tensorflow/core/user_ops/dijkstra_grid_gpu.cu.cc
+++ test_dijkstra_cpu_gpu/tensorflow/core/user_ops/dijkstra_grid_gpu.cu.cc
@@ -14,10 +14,10 @@
     __device__ int get_x(int rank, int * vars_d) { return -1 + rank - (vars_d[SIZEX] * (  (int) ( rank / vars_d[SIZEX] ) )) ; } //-1
     __device__ int get_y(int rank, int * vars_d) { return 0 +  (int) rank / vars_d[SIZEX]  ; } //0
     __device__ int get_rank(int x, int y, int * vars_d) {return 1 + ( (y ) * vars_d[SIZEX] ) + x  ; } // +1
+    __device__ bool is_filled(int * mask, int size) {bool value = true; for(int i = 0; i < size; i ++) {if( mask[i] == UNDEFINED) value = false; } ; return value;}
     
     
-    
-    __device__ void lock(int * mutex) { while (atomicCAS(mutex, 0, 1) != 0  ) ; }
+    __device__ void lock(int * mutex) { while (atomicCAS(mutex, 0, 1) != 0  ) ; } //!= 0
     __device__ void unlock(int * mutex) { atomicExch(mutex, 0); } 
     __device__ void fence(int * mutex, int num) { atomicAdd(mutex, 1) ; while( atomicCAS(mutex, num, num) < num);  }
     
@@ -47,7 +47,7 @@
         
         //lock(&lock2_d[rank]);
         
-        if (mask_d[rank] == WALL){
+        if (mask_d[rank] == WALL || mask_d[rank] != UNDEFINED){
             return false;
         }
         
@@ -94,9 +94,9 @@
     __device__ void must_check(int test , int rank, VARS_SIGNATURE_DECLARE) {
     
         
-
+        //lock(&lock2_d[test]);
             
-        if (   (mask_d[rank] != WALL && mask_d[test] != WALL) ) {
+        if ( true ||  (mask_d[rank] != WALL && mask_d[test] != WALL) ) {
             
             /*
             if ( test == get_rank( vars_d[STOPX], vars_d[STOPY] , vars_d)  && mask_d[test] == UNDEFINED && false ) {
@@ -106,20 +106,21 @@
             }
             */
             
-            lock(&lock1_d[test]);
-            lock(&lock2_d[rank]);
+            //lock(&lock1_d[test]);
+            //lock(&lock2_d[test]);
             
-            bool undef = (mask_d[test] == UNDEFINED); //too many zeros
+            bool undef = (mask_d[test] == UNDEFINED); 
             float a = get_a(test, rank, grid_d);
             int old = dist_d[test];
             int d =  dist_d[rank] + (int) a;
-            bool rank_test = true;//(mask_d[rank] != UNDEFINED);
+            bool rank_test = true;// (mask_d[rank] != UNDEFINED);
             
+            //unlock(&lock2_d[test]);
             
             //lock(&lock_d[test]); // 
-            //if ((d <= dist_d[test] || dist_d[test] == 0   )  && mask_d[test] == UNDEFINED && undef ){
+            
             if (( d <= old || old == 0 ) && undef && rank_test){
-                if (! wall_found( test, rank, VARS_SIGNATURE_CALL) ) {
+                if ( true || (! wall_found( test, rank, VARS_SIGNATURE_CALL) && near_visited(-1, rank, VARS_SIGNATURE_CALL))) {
                     
                     atomicExch(&prev_d[test], rank);
                     atomicExch(&dist_d[test], d);
@@ -129,10 +130,10 @@
                 }
             }
 
-            unlock(&lock1_d[test]);
-            unlock(&lock2_d[rank]);
+            //unlock(&lock2_d[test]);
+
         }
-        //unlock(&lock_d[test]);
+        //unlock(&lock2_d[test]);
     }
     
     
@@ -149,34 +150,20 @@ __global__ void DijkstraGridGpu( VARS_SIGNATURE_DECLARE )  {
         
     int left,right,up,down;
     
-    int sync = 1;
+    //int sync = 1;
     
     int size_x = vars_d[SIZEX];
     int size_y = vars_d[SIZEY];
     
     while(  vars_d[FOUND] == 0 && vars_d[STEP] < vars_d[SIZEX] *  vars_d[SIZEY]  +1) {
         
-        /*
-        left = 0;
-        right = 0;
-        up = 0;
-        down = 0;
-        */
         
-        //int rank = threadIdx.x + blockIdx.x * blockDim.x;
         
-        //vars_d[FENCE2] = 0;
+        if (rank == 0 || true) vars_d[STEP] ++;
         
-        if (rank == 0 || false) vars_d[STEP] ++;
         
-        //prev_d[rank] = 1;
         
-        //lock(&lock_d[rank]);
-        
-        //int size_x = vars_d[SIZEX];
-        //int size_y = vars_d[SIZEY];
-        
-        if (rank < vars_d[SIZEX] * vars_d[SIZEY] +1 ){ 
+        if (rank < vars_d[SIZEX] * vars_d[SIZEY]  ){ 
             
         
             if ( false || vars_d[FOUND] == 0) { 
@@ -236,11 +223,11 @@ __global__ void DijkstraGridGpu( VARS_SIGNATURE_DECLARE )  {
                     if (near_visited(-1, rank, VARS_SIGNATURE_CALL) ){
                         
                         //lock(&lock_d[rank]);
-                        if (false || ( mask_d[rank] == UNDEFINED && mask_d[rank] != WALL)) {
-                            //mask_d[rank] = vars_d[STEP];
+                        if (false || ( mask_d[rank] == UNDEFINED && mask_d[rank] != WALL) ){// || is_filled(mask_d, size_x * size_y)) {
+                            
                             atomicExch(&mask_d[rank ], vars_d[STEP]);
                         }
-                        //unlock(&lock_d[rank]);
+                        
                     }
                     /////////////////////////////
                 }// if 1
@@ -251,135 +238,159 @@ __global__ void DijkstraGridGpu( VARS_SIGNATURE_DECLARE )  {
     fence(&vars_d[FENCE1], (size_x * size_y ) );//* (vars_d[STEP]) );
     vars_d[STEP] = 0;
     
+    
+    
     if (mask_d[rank] != WALL) mask_d[rank] = UNDEFINED;  
+    
+    mask_d[get_rank( vars_d[STARTX], vars_d[STARTY], vars_d) ] = VISITED;
+    prev_d[get_rank( vars_d[STARTX], vars_d[STARTY], vars_d) ] = -1;
+    dist_d[get_rank( vars_d[STARTX], vars_d[STARTY], vars_d) ] = 1;
+    
+    int count;
                     
-    while( vars_d[FOUND] == 0 && vars_d[STEP] < vars_d[SIZEX] *  vars_d[SIZEY]  +1) {
+    while( vars_d[FOUND] == 0 && vars_d[STEP] < vars_d[SIZEX] *  vars_d[SIZEY]  ) {
+        
         
         left = 0;
         right = 0;
         up = 0;
         down = 0;
         
-        //vars_d[FENCE2] = 0;
+        
+        count = 0;
         
         if (rank == 0 || false) vars_d[STEP] ++;
         
         
-        if (rank < vars_d[SIZEX] * vars_d[SIZEY] + 1 ){ 
+        if (rank < vars_d[SIZEX] * vars_d[SIZEY]  ){ 
             
         
             if ( false || vars_d[FOUND] == 0) { 
             
-                
+                //lock(&lock1_d[rank]);
                 if ( ( mask_d[rank] != WALL ) ){ // && mask_d[rank] != UNDEFINED ) { 
                     
                     /////////////////////////////
                     //if (sync == 1) __syncthreads();
 
-                    //fence(&vars_d[FENCE1], (size_x * size_y ) * (vars_d[STEP]) );
-                    //if (rank == 0) atomicExch(&vars_d[FENCE1], 0);
-                    
-                    //right
-                    if (get_y(rank, vars_d) == get_y(rank + 1, vars_d) && 
-                            rank + 1 < size_x * size_y && near_visited(- 1, rank, VARS_SIGNATURE_CALL) ) { 
-                        
-                        if (! wall_found(rank+1,rank, VARS_SIGNATURE_CALL)) {
-                            
-                            right = 1;
-                            must_check(rank + 1, rank, VARS_SIGNATURE_CALL) ;
+                    while (count < 4 && vars_d[FOUND] < 1 ) {
+                        //right
+                        if (right == 0) {
+                            if (get_y(rank, vars_d) == get_y(rank + 1, vars_d) && 
+                                    rank + 1 < size_x * size_y && near_visited(- 1, rank, VARS_SIGNATURE_CALL) ) { 
+                                
+                                if ( lock2_d[rank + 1] == 0 ) {
+                                    
+                                    //right = 1;
+                                    must_check(rank + 1, rank, VARS_SIGNATURE_CALL) ;
+                                    //count ++;
+                                    right = 1;
+                                }
+                                
+                            }
+                            else {
+                                //count ++;
+                                right = 1;
+                            }
                         }
                         
-                    }
-                    
-                    //left
-                    if (get_y(rank, vars_d) == get_y(rank - 1, vars_d) && 
-                            rank - 1 >= 0 && near_visited(-1, rank, VARS_SIGNATURE_CALL) ){
-                            
-                        
-                        if (! wall_found(rank -1, rank, VARS_SIGNATURE_CALL)) {
-                            
-                            left = 1;
-                            must_check(rank -1, rank, VARS_SIGNATURE_CALL);
+                        //left
+                        if (left == 0) {
+                            if (get_y(rank, vars_d) == get_y(rank - 1, vars_d) && 
+                                    rank - 1 >= 0 && near_visited(-1, rank, VARS_SIGNATURE_CALL) ){
+                                    
+                                
+                                if (lock2_d[rank - 1] == 0 ) {
+                                    
+                                    //left = 1;
+                                    must_check(rank -1, rank, VARS_SIGNATURE_CALL);
+                                    //count ++;
+                                    left = 1;
+                                }
+                                
+                            }
+                            else {
+                                //count ++;
+                                left = 1;
+                            }
                         }
                         
-                    }
-                    
-                    //down
-                    if (rank + size_x < size_x * size_y && near_visited(-1, rank, VARS_SIGNATURE_CALL) ){
-                        
-                        if(! wall_found(rank+ size_x, rank, VARS_SIGNATURE_CALL) ) {
-                            
-                            down = 1;
-                            must_check(rank + size_x, rank, VARS_SIGNATURE_CALL);
+                        //down
+                        if (down == 0) {
+                            if (rank + size_x < size_x * size_y && near_visited(-1, rank, VARS_SIGNATURE_CALL) ){
+                                
+                                if( lock2_d[rank + size_x] == 0 ) {
+                                    
+                                    //down = 1;
+                                    must_check(rank + size_x, rank, VARS_SIGNATURE_CALL);
+                                    //count ++;
+                                    down = 1;
+                                }
+                                
+                            }
+                            else {
+                                //count ++;
+                                down = 1;
+                            }
                         }
                         
-                    }
-                    
-                    //up
-                    if (rank - size_x >=0 && near_visited(-1, rank, VARS_SIGNATURE_CALL) ) { 
-                        
-                        if (! wall_found(rank - size_x, rank, VARS_SIGNATURE_CALL) ) {
-                        
-                            up = 1;
-                            must_check(rank - size_x, rank, VARS_SIGNATURE_CALL);
+                        //up
+                        if (up == 0) {
+                            if (rank - size_x >=0 && near_visited(-1, rank, VARS_SIGNATURE_CALL) ) { 
+                                
+                                if ( lock2_d[rank - size_x] == 0  ) {
+                                
+                                    //up = 1;
+                                    must_check(rank - size_x, rank, VARS_SIGNATURE_CALL);
+                                    //count ++;
+                                    up = 1;
+                                }
+                                
+                            }
+                            else {
+                                //count ++;
+                                up = 1;
+                            }
                         }
-                        
+                        count = left + right + up + down;
                     }
                     /////////////////////////
                     
-                    /////////////////////////
-                    if (sync == 1) __syncthreads();
                     
-                    //fence(&vars_d[FENCE2], (size_x * size_y) * (vars_d[STEP] ) );
-                    //if (rank == 0) atomicExch(&vars_d[FENCE2], 0);
-                    //lock(&lock_d[rank]); // 
-                    if(right == 1) {
-                        //must_check(rank + 1, rank, VARS_SIGNATURE_CALL) ;
-                    }
-                    if(up == 1) {
-                        //must_check(rank - size_x, rank, VARS_SIGNATURE_CALL);
-                    }
-                    if(left == 1) {
-                        //must_check(rank -1, rank, VARS_SIGNATURE_CALL);
-                    }
-                    if(down == 1) {
-                        //must_check(rank + size_x, rank, VARS_SIGNATURE_CALL);
-                    }
-                    
-                    //lock(&lock_d[rank]);
-                    ////////////////////////
-                    //fence(&vars_d[FENCE3], (size_x * size_y) * (vars_d[STEP] ) );
-                    
-                    if (near_visited(-1, rank, VARS_SIGNATURE_CALL) ){
-                        
-                        //lock(&lock_d[rank]);
-                        if (false || ( mask_d[rank] == UNDEFINED && mask_d[rank] != WALL)) {
-                            //mask_d[rank] = vars_d[STEP];
-                            atomicExch(&mask_d[rank ], vars_d[STEP]);
-                        }
-                        //unlock(&lock_d[rank]);
-                    }
-                    //unlock(&lock_d[rank]); // 
-                    if ( rank == get_rank( vars_d[STOPX], vars_d[STOPY] , vars_d) && near_visited(-1, rank, VARS_SIGNATURE_CALL)  && 
-                            //mask_d[rank] == UNDEFINED && 
-                            mask_d[rank] != WALL) {
+                    if ( (rank == get_rank( vars_d[STOPX], vars_d[STOPY] , vars_d) && near_visited(-1, rank, VARS_SIGNATURE_CALL)  && 
+                            mask_d[rank] == UNDEFINED && 
+                            mask_d[rank] != WALL) ){//|| is_filled(mask_d, size_x * size_y) ) {
                         if ( true ) {
-                            //vars_d[FOUND] = FOUND_CONST;
+
 
                             atomicExch(&vars_d[FOUND], FOUND_CONST);
                         }
                         
                     }
                     
+                    if (near_visited(-1, rank, VARS_SIGNATURE_CALL) ){
+                        
+
+                        if (false || ( 
+                            mask_d[rank] == UNDEFINED && 
+                            mask_d[rank] != WALL)) {
+                            
+                            atomicExch(&mask_d[rank ], vars_d[STEP]);
+                        }
+
+                    }
+
+                    
+                    
 
                     
                 }
-
+                //unlock(&lock1_d[rank]);
             }
 
         } // if
         
-        //unlock(&lock_d[rank]);
+
 
     } // while
     
@@ -445,7 +456,8 @@ __global__ void DijkstraGridGpu( VARS_SIGNATURE_DECLARE )  {
         printf("start spot %i \n", mask[size_x * vars[STARTY] + vars[STARTX]  ]);
         
         for (int i = 0; i < size; i ++) {
-            printf(",%i ", prev[i]);
+            printf(",%i ", mask[i]);
+            if (i % size_x == size_x -1) printf("\n");
         }
         printf(" size %i \n",size);
         
