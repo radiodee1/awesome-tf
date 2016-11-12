55ea22a147deab518079551f062d7ef6f388d009
David Liebman <radiodee1@gmail.com>
2016-11-12 11:28:06 -0500

    update tf url for pip

--- do_update_tf.sh
+++ do_update_tf.sh
@@ -1,4 +1,4 @@
-export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc2-cp27-none-linux_x86_64.whl
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0-cp27-none-linux_x86_64.whl
 
 sudo pip install --upgrade $TF_BINARY_URL
 
