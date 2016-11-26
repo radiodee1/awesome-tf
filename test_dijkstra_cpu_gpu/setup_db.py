ee6a587dd049ee018d35b91e9725865f74a90278
David Liebman <radiodee1@gmail.com>
2016-11-26 09:58:43 -0500

    working on gui

--- test_dijkstra_cpu_gpu/setup_db.py
+++ test_dijkstra_cpu_gpu/setup_db.py
@@ -15,6 +15,11 @@ class Interface(Gtk.Window) :
         self.mapname = ""
         self.quit = 0
         
+        self.originalarray =  [(0,"img/map.png" , "first map"),
+                        (1,"img/smallmap.png","second map"),
+                        (2,"img/maze.png", "maze"),
+                        (5,"img/notthere.png","not there")]
+        
         self.testarray =     [(0,"img/map.png" , "first map"),
                         (1,"img/smallmap.png","second map"),
                         (2,"img/maze.png", "maze"),
@@ -26,18 +31,19 @@ class Interface(Gtk.Window) :
         self.grid = Gtk.Grid()
         self.add(self.grid)
         
-        self.button = Gtk.Button(label="Load Image")
+        self.button = Gtk.Button(label="Load PNG Image")
         self.button.connect("clicked", self.on_image_button_clicked)
         self.grid.add(self.button)
         
-        self.label = Gtk.Label("filename")
+        self.label = Gtk.Label("display-name")
         self.grid.attach(self.label, 0, 1, 1, 1)
         
         self.entry = Gtk.Entry()
-        self.entry.set_text("mapname")
+        self.entry.set_text("")
+        self.entry.connect("activate", self.enter_callback)
         self.grid.attach(self.entry, 1, 1, 1, 1)
         
-        self.confirm = Gtk.Button(label="Save Image")
+        self.confirm = Gtk.Button(label="Save/Send Image")
         self.confirm.connect("clicked", self.on_confirm_button_clicked)
         self.grid.attach(self.confirm, 1, 0, 1, 1)
         
@@ -55,7 +61,21 @@ class Interface(Gtk.Window) :
         self.treeview.append_column(column1)
         self.treeview.append_column(column2)
         
+        self.select = self.treeview.get_selection()
+        self.select.connect("changed", self.on_selection_changed)
+        
         self.grid.attach(self.treeview, 0,2,3,3)
+        
+    def enter_callback(self, widget):
+        print (self.entry.get_text())
+        self.label.set_text(self.entry.get_text())
+        
+    def on_selection_changed(self, widget):
+        model, treeiter = widget.get_selected()
+        if treeiter != None:
+            print("selected", model[treeiter][0] )
+            self.mapname = self.originalarray[int(model[treeiter][0])][1]
+            print (self.mapname)
         
     def on_image_button_clicked(self, widget):
         print("Hello World")
@@ -63,9 +83,11 @@ class Interface(Gtk.Window) :
         self.mapname = askopenfilename() 
         print(self.mapname)
         if len(self.mapname) != 0:
-            self.label.set_text('...' + self.mapname[len(self.mapname) - 10 :])
-        self.testarray.append((9, self.mapname, self.mapname))
+            self.label.set_text('...' + self.mapname[ - 10 :])
+            self.testarray.append((9,'...'+ self.mapname[-10: ],'...'+ self.mapname[-10:]))
+            self.originalarray.append( (9,  self.mapname, self.mapname ) )
         print(self.testarray)
+        print(self.originalarray)
         self.liststore = self.make_list()
         self.treeview.set_model(self.liststore)
         
